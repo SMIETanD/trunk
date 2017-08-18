5e8c55aba98ae9c6ed901b0217948cf666c4cbc9
XrLee <715169084@qq.com>
2017-08-18 17:19:44 +0800

    test01

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use(express.static(path.join(__dirname, 'app')));
 
 app.use('/', routes);
 
-//server.listen(app.get('port'), function () {
-//    console.log('Express server listening on port ' + app.get('port'));
-//});
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
 
 module.exports = app;
